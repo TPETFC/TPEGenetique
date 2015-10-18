b3f4673d58d680b01ebbbf023ce3f139dd635cf5
DTristan <chancyleg@gmail.com>
2015-10-18 14:10:15 +0200

    Frame

--- src/com/tpe/genetique/core/Frame.java
+++ src/com/tpe/genetique/core/Frame.java
@@ -1,6 +1,9 @@
 package com.tpe.genetique.core;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import com.tpe.genetique.util.Constants;
 
 public class Frame extends JFrame {
 
@@ -10,6 +13,18 @@ public class Frame extends JFrame {
 	public Frame(int screenWidth, int screenHeight) {
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
+		this.initFrame();
+	}
+	private void initFrame(){
+		if(screenWidth < 800 || screenHeight < 600){
+			JOptionPane.showMessageDialog(null, "La résolution de votre moniteur ne permet pas l'exécution de ce programme","Erreur",JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		this.setTitle(Constants.TITLE);
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);
+		this.setResizable(true);
+		this.setVisible(true);
 	}
 
 }
---
+++ src/com/tpe/genetique/util/Constants.java
@@ -0,0 +1,7 @@
+package com.tpe.genetique.util;
+
+public class Constants {
+	
+	public static final String TITLE = "TPE";
+
+}
